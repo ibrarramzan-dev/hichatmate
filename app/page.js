b014a6cff46b047b0d40ef119072339fccb4cd24
Ibrar Ramzan <ibrarramzan-dev@gmail.com>
2025-04-06 03:25:43 -0400

    next app setup

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <header>This is a header</header>
+      <main>Hello world!</main>
+      <footer>This is a footer</footer>
+    </div>
+  );
+}
